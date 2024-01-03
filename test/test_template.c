3b2e52c9740eecb88da3f7f1874badbd5f535ed5
ThunderSphun <manderijn2000@gmail.com>
2024-01-03 20:41:53 +0100

    added a template file for testing, just copy and go

---
+++ test/test_template.c
@@ -0,0 +1,15 @@
+#include /* enter header to test */
+
+#include "unity_wrapper.h"
+
+// included to prevent compilation error in some cases
+void setUp(void) {}
+void tearDown(void) {}
+
+int main() {
+	UNITY_BEGIN();
+
+
+
+	return UNITY_END();
+}
